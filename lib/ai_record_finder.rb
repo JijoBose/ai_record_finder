05b0029e3d8531c78099e303f30667dd301915dc
Jijo Bose <bosejijo@gmail.com>
2026-02-19 17:03:46 +0530

    Initial commit

---
+++ lib/ai_record_finder.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ai_record_finder/version"
+
+module AiRecordFinder
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ai_record_finder/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AiRecordFinder
+  VERSION = "0.1.0"
+end
